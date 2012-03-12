2248f33f9ec6cdc11005420ff151ee17e53b8c10
Guillaume Filion <guillaume.filion@gmail.com>
2012-03-12 23:48:55 +0100

    Turn the cursor to a hand over 'folded_hard_parts'.

--- js/hardparts.js
+++ js/hardparts.js
@@ -13,10 +13,16 @@ function there_are_hard_parts() {
 }
 
 function initialize_hard_parts_display() {
-  $('<img class="folded_hard_part centered" src="/img/moebius.png">').clone().insertBefore('.hard');
+  // Create and insert the image.
+  $('<img class="folded_hard_part centered" src="/img/moebius.png">').
+	  clone().
+	  insertBefore('.hard');
+  // Register click event handler.
   $('.folded_hard_part').click(function() {
     $(this).hide();
     $(this).next().show(300);
   });
+  // Register hover event handler (hand cursor when hovering).
+  // Hide the hard parts.
   $('.hard').hide();
 }
