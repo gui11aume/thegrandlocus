cb82f4daff64b83c263ad6de385fff5b0cec3964
Guillaume Filion <guillaume.filion@gmail.com>
2012-03-22 17:52:50 +0100

    Changed Django version to 1.2. and fixed error messages.

--- setup_django_version.py
+++ setup_django_version.py
@@ -1,6 +1,5 @@
 #! -*- coding: utf-8 -*-
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings' 
 
 from google.appengine.dist import use_library
-use_library('django', '1.3')
+use_library('django', '1.2')
